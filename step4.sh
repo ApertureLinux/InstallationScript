71bdfba8961b5554a19d97fdb5557499ea288166
anjirocool <anjirocool@gmail.com>
2020-10-06 22:57:07 -0400

    Moved post-chroot stuff to a new step4.sh file so that the script continues inside chroot

---
+++ step4.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Set timezone
+clear
+ls /usr/share/zoneinfo
+echo "Choose your region"
+read userregion
+clear
+ls /usr/share/zoneinfo/$userregion
+echo "Choose a city that shares your time zone"
+read usercity
+ln -sf /usr/share/zoneinfo/$userregion/$usercity /etc/localtime
+hwclock --systohc
+
+#localization
+#TODO: locale choice
+userlocale1="en_US.UTF-8"
+userlocale2="en_US ISO-8859-1"
+
+sed -i "/^#.* $userlocale1 /s/^#//" /etc/locale.gen
+sed -i "/^#.* $userlocale2 /s/^#//" /etc/locale.gen
+locale-gen
+echo "LANG=$userlocale1" > /etc/locale.conf
+
+#hostname
+echo "What will you name your computer?"
+read hostname
+echo $hostname > /etc/hostname
+
+clear
+echo "Select a root password."
+passwd
